f5247b231d65ba670df488687b1d84e3000e7466
Rudi Kreidenhuber <Rudi.Kreidenhuber@gmail.com>
2021-11-09 20:33:21 +0100

    Working clinical version 09_11_21

---
+++ Start_VEEG_pipeline_clinic.bat
@@ -0,0 +1,6 @@
+@echo on
+call C:\Users\User\anaconda3\Scripts\activate.bat
+# call conda activate mne023_4
+cd .\src\
+jupyter notebook VEEG.ipynb
+pause
--- process_veeg.bat
+++
@@ -1,6 +0,0 @@
-@echo on
-call C:\Users\rudik\miniconda3\Scripts\activate.bat
-call conda activate mne023_4
-cd .\src\
-jupyter notebook VEEG.ipynb
-pause
